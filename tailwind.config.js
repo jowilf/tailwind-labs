012aa2db6dee18d999b40d6b81666caa64d8dbf8
Jocelin HOUNON <hounonj@gmail.com>
2021-06-08 18:18:09 +0100

    end

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,25 @@
 module.exports = {
-    purge: [],
-    darkMode: false, // or 'media' or 'class'
-    theme: {
-        extend: {},
-    },
-    variants: {
-        extend: {
-            backgroundColor: ['active']
+  mode: "jit",
+  purge: ["index.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          DEFAULT: "#0fa9e6",
+          light: "#3fbaeb",
+          dark: "#0c87b8",
         },
+      },
+      fontFamily: {
+        headline: "Poppins, sans-serif",
+      },
     },
-    plugins: [],
-}
+  },
+  variants: {
+    extend: {
+      backgroundColor: ["active"],
+    },
+  },
+  plugins: [],
+};
